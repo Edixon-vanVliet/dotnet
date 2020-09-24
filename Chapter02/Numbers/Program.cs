93a4e58d508f2d19ded61b01499ac4bea320881d
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 15:03:49 -0400

    Declare integer

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -9,6 +9,10 @@ namespace Numbers
             // unsigend integer means positive whole number
             // including 0
             uint naturalNumber = 23;
+
+            // integer means negative or positive whole number
+            // including 0
+            int integerNumber = -23;
         }
     }
 }
