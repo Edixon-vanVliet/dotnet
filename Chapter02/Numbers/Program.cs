a9770ebb059ec8d0cc6eb9ac9f28e7d8c2c769ad
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 15:46:37 -0400

    Compare double and decimal types

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -36,6 +36,26 @@ namespace Numbers
             Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
 
             Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+
+            Console.WriteLine("Using double");
+
+            double a = 0.1;
+            double b = 0.2;
+
+            if (a + b == 0.3)
+                Console.WriteLine($"{a} + {b} equals 0.3");
+            else
+                Console.WriteLine($"{a} + {b} does NOT equals 0.3");
+
+            Console.WriteLine("Using decimal");
+
+            decimal c = 0.1m; // m suffix means a decimal literal value
+            decimal d = 0.2m;
+
+            if (c + d == 0.3m)
+                Console.WriteLine($"{c} + {d} equals 0.3");
+            else
+                Console.WriteLine($"{c} + {d} does NOT equals 0.3");
         }
     }
 }
