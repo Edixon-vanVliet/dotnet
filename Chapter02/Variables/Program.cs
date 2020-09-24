9bfe3784f3682cde7c7977c48fd6e0dd4c7c1d7b
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 16:09:56 -0400

    Modify the previous strong typed statements to use var

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -24,12 +24,12 @@ namespace Variables
             // property named Length
             int length = anotherName.Length;
 
-            int population = 66_000_000; // 66 million in UK
-            double weight = 1.88; // in kilograms
-            decimal price = 4.99M; // in pounds sterling
-            string fruit = "Apples"; // strings use double-quotes
-            char letter = 'Z'; // chars use single-quotes
-            bool happy = true; // Booleans have value of true or false
+            var population = 66_000_000; // 66 million in UK
+            var weight = 1.88; // in kilograms
+            var price = 4.99M; // in pounds sterling
+            var fruit = "Apples"; // strings use double-quotes
+            var letter = 'Z'; // chars use single-quotes
+            var happy = true; // Booleans have value of true or false
         }
     }
 }
