36f2dd62c1eed74d8982e937565d5c2bcefb0e59
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 15:35:59 -0400

    Display size and range of decimal

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -34,6 +34,8 @@ namespace Numbers
             Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers int the range {int.MinValue:N0} to {int.MaxValue:N0}.");
 
             Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers int the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+
+            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers int the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
         }
     }
 }
