bfb25d30f0ddf6e9c73ff12c54cca44807bce170
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 16:01:25 -0400

    Use object

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -6,7 +6,15 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            object height = 1.75; // storing a double in an object
+            object name = "Edixon"; // storing a string in an object
+
+            Console.WriteLine($"{name} is {height} metres tall.");
+
+            // int lenght1 = name.Length; // gives compile error
+            int length2 = ((string)name).Length; // tell compiler it is a string
+
+            Console.WriteLine($"{name} has {length2} characters.");
         }
     }
 }
