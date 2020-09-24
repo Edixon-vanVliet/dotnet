d44eb03c2e7263525659099af5c790cdf0530510
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 16:15:05 -0400

    Display default value of string

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -34,6 +34,7 @@ namespace Variables
             Console.WriteLine($"default(int) = {default(int)}");
             Console.WriteLine($"default(bool) = {default(bool)}");
             Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+            Console.WriteLine($"default(string) = {default(string)}");
         }
     }
 }
