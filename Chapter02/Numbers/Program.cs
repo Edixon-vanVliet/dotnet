33acd50fbe3f20cf7ffc969c48cd89cf8e063b02
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 15:12:32 -0400

    Declare 3 variables that store the number 2 million in decimal, binary and hexadecimal notation

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -20,6 +20,11 @@ namespace Numbers
 
             // double means double-precision floating point
             double anotherRealNumber = 2.3; // double literal
+
+            // three variables that store the number 2 million
+            int decimalNotation = 2_000_000;
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
         }
     }
 }
