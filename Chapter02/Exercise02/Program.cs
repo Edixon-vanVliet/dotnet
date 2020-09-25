24785e9f55c78e32c0f7fd04d01a7596be5d32a8
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 21:33:50 -0400

    Write exercise

--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using static System.Console;
 
+/// <summary>
+/// Exercise that outputs the number of bytes in memory that each of the
+/// following number types use, and the minimum and maximum values they can
+/// have: sbyte, byte, short, ushort, int, uint, long, ulong, float, double and
+/// decimal
+/// </summary>
+
 namespace Exercise02
 {
     class Program
