629149d94d7bc727713b8533c4695082474be511
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-09-24 20:51:14 -0400

    Import static System.Console

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Formatting
 {
@@ -9,7 +10,7 @@ namespace Formatting
             int numberOfApples = 12;
             decimal pricePerApple = 0.35m;
 
-            Console.WriteLine(
+            WriteLine(
                 format: "{0} apples costs {1:C}",
                 arg0: numberOfApples,
                 arg1: numberOfApples * pricePerApple
@@ -23,38 +24,38 @@ namespace Formatting
 
             // WriteToFile(formatted); // writes the string into a file
 
-            Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+            WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
 
             string applesText = "Apples";
             int applesCount = 1234;
             string bananasText = "Bananas";
             int bananasCount = 56789;
 
-            Console.WriteLine(
+            WriteLine(
                 format: "{0,-8} {1,6:N0}",
                 arg0: "Name",
                 arg1: "Count"
             );
 
-            Console.WriteLine(
+            WriteLine(
                 format: "{0,-8} {1,6:N0}",
                 arg0: applesText,
                 arg1: applesCount
             );
 
-            Console.WriteLine(
+            WriteLine(
                 format: "{0,-8} {1,6:N0}",
                 arg0: bananasText,
                 arg1: bananasCount
             );
 
-            Console.Write("Type your first name and press ENTER: ");
-            string firstName = Console.ReadLine();
+            Write("Type your first name and press ENTER: ");
+            string firstName = ReadLine();
 
-            Console.Write("Type you age and press ENTER: ");
-            string age = Console.ReadLine();
+            Write("Type you age and press ENTER: ");
+            string age = ReadLine();
 
-            Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+            WriteLine($"Hello {firstName}, you look good for {age}.");
         }
     }
 }
