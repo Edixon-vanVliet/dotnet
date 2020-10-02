a7da36169c1fc147631894a06b1fe31d178fb82e
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-02 13:11:53 -0400

    Multiply a by 8

--- Chapter03/BitwiseAndShiftOperators/Program.cs
+++ Chapter03/BitwiseAndShiftOperators/Program.cs
@@ -19,6 +19,9 @@ namespace BitwiseAndShiftOperators
 
             // 0101 0000 left-shift a by three bit columns
             WriteLine($"a << 3 = {a << 3}");
+
+            // multiply a by 8
+            WriteLine($"a * 8 = {a * 8}");
         }
     }
 }
