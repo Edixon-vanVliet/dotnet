56be1e86a4d8adc9e9fb63e18e9198972ccbcb3c
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 17:02:44 -0400

    Get two numbers of the user in the range of 0-255 and divide the first number by the second, handle all exceptions

--- Chapter03/Exercise04/Program.cs
+++ Chapter03/Exercise04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 /// <summary>
 /// asks the user for two numbers in the range 0-255 and then divides 
@@ -11,7 +12,30 @@ namespace Exercise04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal number1, number2;
+
+            try
+            {
+                Write("Enter a number between 0 and 255: ");
+                number1 = byte.Parse(ReadLine());
+
+                Write("Enter a number between 0 and 255: ");
+                number2 = byte.Parse(ReadLine());
+
+                WriteLine($"{number1} divided by {number2} is {number1 / number2}");
+            }
+            catch (OverflowException)
+            {
+                WriteLine("number to big or to small.");
+            }
+            catch (FormatException)
+            {
+                WriteLine("Not a valid number.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{ex.GetType()} says {ex.Message}.");
+            }
         }
     }
 }
