0e711b3cd515d3b4202988aed5ffc4620bb292b2
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 16:04:49 -0400

    Catch OverflowException

--- Chapter03/CheckingForOverflow/Program.cs
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -7,19 +7,26 @@ namespace CheckingForOverflow
     {
         static void Main(string[] args)
         {
-            checked
+            try
             {
-                int x = int.MaxValue - 1;
-                WriteLine($"Initial value: {x}");
+                checked
+                {
+                    int x = int.MaxValue - 1;
+                    WriteLine($"Initial value: {x}");
 
-                x++;
-                WriteLine($"After incrementing: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
 
-                x++;
-                WriteLine($"After incrementing: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
 
-                x++;
-                WriteLine($"After incrementing: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
+                }
+            }
+            catch (OverflowException)
+            {
+                WriteLine("The code overflowed but I caught the exception.");
             }
         }
     }
