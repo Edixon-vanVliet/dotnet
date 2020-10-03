02581eabfb3cfa92682bbe9c9675e93a5c72d347
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 16:50:29 -0400

    Add summary of the exercise

--- Chapter03/Exercise04/Program.cs
+++ Chapter03/Exercise04/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+/// <summary>
+/// asks the user for two numbers in the range 0-255 and then divides 
+/// the first number by the second
+/// </summary>
+
 namespace Exercise04
 {
     class Program
