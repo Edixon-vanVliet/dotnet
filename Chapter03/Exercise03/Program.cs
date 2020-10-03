efa7bb22c20fb6eb9a9149614ecaf9e3f1bf5e77
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 16:22:48 -0400

    Add summary of the exercise

--- Chapter03/Exercise03/Program.cs
+++ Chapter03/Exercise03/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using static System.Console;
+
+/// <summary>
+/// Simulated FizzBuzz game counting up to 100
+/// when the count is divisible by three replace with the word fizz
+/// when the count is divisible by five replace with the word buzz
+/// when the count is divisible by three and five replace with the word fizzbuzz
+/// </summary>
 
 namespace Exercise03
 {
