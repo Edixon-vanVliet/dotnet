91ad443160c146f957d7125d5faba475eed15485
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 13:24:33 -0400

    Simplify switch with switch expressions

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -85,6 +85,22 @@ namespace SelectionStatements
             }
 
             WriteLine(message);
+
+            message = s switch
+            {
+                FileStream writeableFile when s.CanWrite
+                    => "The stream is a file that I can write to.",
+                FileStream readOnlyFile
+                    => "The stream is a read-only file.",
+                MemoryStream ms
+                    => "The stream is a memory address.",
+                null
+                    => "The stream is null.",
+                _
+                    => "The stream is some other type."
+            };
+
+            WriteLine(message);
         }
     }
 }
