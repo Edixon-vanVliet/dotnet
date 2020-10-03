a06f09e7294ac1633bf87c46f498a611fa18a42e
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 15:25:52 -0400

    Use TryParse()

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -87,6 +87,20 @@ namespace CastingConverting
             WriteLine($"I was born {age} years ago.");
             WriteLine($"My birthday is {birthday}.");
             WriteLine($"My birthday is {birthday:D}.");
+
+            Write("How many eggs are there? ");
+
+            int count;
+            string input = Console.ReadLine();
+            
+            if (int.TryParse(input, out count))
+            {
+                WriteLine($"There are {count} eggs.");
+            }
+            else
+            {
+                WriteLine("I could not parse the input.");
+            }
         }
     }
 }
