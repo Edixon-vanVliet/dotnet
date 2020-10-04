de59f6781c1d36a0e1822379080861e69229033c
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 22:15:15 -0400

    Create function TimesTable

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using static System.Console;
 
 namespace WritingFunctions
 {
     class Program
     {
+        static void TimesTable(byte number)
+        {
+            WriteLine($"This the {number} times table:");
+
+            for (int row = 1; row <= 12; row++)
+            {
+                WriteLine($"{row} x {number} = {row * number}");
+            }
+
+            WriteLine();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
