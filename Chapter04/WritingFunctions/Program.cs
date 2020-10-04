5e0b17408c060288834f66c5248e2709d99d0491
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-04 14:38:40 -0400

    Create function Factorial

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -141,6 +141,22 @@ namespace WritingFunctions
             WriteLine();
         }
 
+        static int Factorial(int number)
+        {
+            if (number < 1)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+
         static void Main(string[] args)
         {
             // RunTimesTable();
