6381004549ef1b46a9f104d4837428b87b7b3dfe
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 22:18:31 -0400

    Create function RunTimesTable

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -16,6 +16,29 @@ namespace WritingFunctions
 
             WriteLine();
         }
+
+        static void RunTimesTable()
+        {
+            bool isNumber;
+
+            do
+            {
+                Write("Enter a number between 0 and 255: ");
+
+                isNumber = byte.TryParse(ReadLine(), out byte number);
+
+                if (isNumber)
+                {
+                    TimesTable(number);
+                }
+                else
+                {
+                    WriteLine("You did not enter a valid number!");
+                }
+            }
+            while (isNumber);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
