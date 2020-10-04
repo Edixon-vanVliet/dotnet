ad1906874f23250e7bc0338175eaee2677a38e6e
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-03 22:54:10 -0400

    Create function CalculateTax

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -39,6 +39,48 @@ namespace WritingFunctions
             while (isNumber);
         }
 
+        static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+        {
+            decimal rate = 0.0m;
+
+            switch (twoLetterRegionCode)
+            {
+                case "CH": // Switzerland
+                    rate = 0.08m;
+                    break;
+                case "DK": // Denmark
+                case "NO": // Normway
+                    rate = 0.25m;
+                    break;
+                case "GB": // United Kingdom
+                case "FR": // France
+                    rate = 0.2m;
+                    break;
+                case "HU": // Hungary
+                    rate = 0.27m;
+                    break;
+                case "OR": // Oregon
+                case "AK": // Alaska
+                case "MT": // Montana
+                    rate = 0.0m;
+                    break;
+                case "ND": // North Dakota
+                case "WI": // Wisconsin
+                case "ME": // Maryland
+                case "VA": // Virginia
+                    rate = 0.05m;
+                    break;
+                case "CA": // California
+                    rate = 0.0825m;
+                    break;
+                default: // most US states
+                    rate = 0.06m;
+                    break;
+            };
+
+            return amount * rate;
+        }
+
         static void Main(string[] args)
         {
             RunTimesTable();
