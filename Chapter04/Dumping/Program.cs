51c2234a5865fa772a273555fd9a7efb1e2c592b
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-04 18:07:51 -0400

    Create an async function that dumps a complex object

--- Chapter04/Dumping/Program.cs
+++ Chapter04/Dumping/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using SharpPad;
+using System.Threading.Tasks;
+using static System.Console;
 
 namespace Dumping
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var complexObject = new
+            {
+                FirstName = "Edixon",
+                Birthday = new DateTime(1996, 3, 27),
+                Friends = new[] { "Bleyly", "Cesar", "Chris" }
+            };
+
+            WriteLine($"Dumping {nameof(complexObject)} to SharpPad.");
+
+            await complexObject.Dump();
         }
     }
 }
