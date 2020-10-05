d44b38ad89d46788ea63444c2d05ce16d6baa673
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-05 12:25:15 -0400

    Write test to add 2 and 2

--- Chapter04/CalculatorLibUnitTests/CalculatorUnitTests.cs
+++ Chapter04/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using CalculatorLib;
 
 namespace CalculatorLibUnitTests
 {
     public class CalculatorUnitTests
     {
         [Fact]
-        public void Test1()
+        public void TestAdding2And2()
         {
+            // arange
+            double a = 2;
+            double b = 2;
+            double expected = 4;
 
+            var calc = new Calculator();
+
+            // act
+            double actual = calc.Add(a, b);
+
+            // assert
+            Assert.Equal(expected, actual);
         }
     }
 }
