00e44c6d87b28b1df2bd5278e1257d8b9ad60244
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-05 12:59:54 -0400

    Renamed class to Primes and add function PrimeFactors

--- Chapter04/PrimeFactorsLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PrimeFactorsLib
-{
-    public class Class1
-    {
-    }
-}
---
+++ Chapter04/PrimeFactorsLib/Primes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeFactorsLib
+{
+    public class Primes
+    {
+        int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+        public string PrimeFactors(int a)
+        {
+            return a.ToString();
+        }
+    }
+}
