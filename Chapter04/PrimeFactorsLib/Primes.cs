dadac20a85d3b4ee82f417558063dde54f68fd6f
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-05 14:09:01 -0400

    Update function to return the prime factors of a number

--- Chapter04/PrimeFactorsLib/Primes.cs
+++ Chapter04/PrimeFactorsLib/Primes.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using System.Linq;
 
 namespace PrimeFactorsLib
 {
     public class Primes
     {
         int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
-        public string PrimeFactors(int a)
+
+        /// <summary>
+        /// Pass a 32-bit integer and it will return its prime factors
+        /// </summary>
+        /// <param name="number">Number is the value you want the prime factors 
+        /// of eg. 50</param>
+        /// <returns>return the prime factors of number eg. 5 x 5 x 2</returns>
+        public string PrimeFactors(int number)
         {
-            return a.ToString();
+            if (primeNumbers.Contains(number))
+            {
+                return number.ToString();
+            }
+
+            foreach (int prime in primeNumbers)
+            {
+                if (number % prime == 0)
+                {
+                    return $"{PrimeFactors(number / prime)} x {prime}";
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
