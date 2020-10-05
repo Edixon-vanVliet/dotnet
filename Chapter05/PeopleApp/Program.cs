76814a43ce8790c5bc8e829f6d433b563a401d34
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-05 16:03:06 -0400

    Assign values to new fields and display them

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -9,7 +9,14 @@ namespace PeopleApp
         static void Main(string[] args)
         {
             var edixon = new Person();
-            WriteLine(edixon.ToString());
+            edixon.Name = "Edixon van Vliet";
+            edixon.DateOfBirth = new DateTime(1996, 3, 27);
+
+            WriteLine(
+                "{0} was born on {1:dddd, d MMMM yyyy}",
+                edixon.Name,
+                edixon.DateOfBirth
+            );
         }
     }
 }
