7a77674e4dfa6a34e0c674d98304e7ac02c48682
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-05 13:03:31 -0400

    Renamed class PrimeFactorsUnitTests and add some tests

---
+++ Chapter04/PrimeFactorsTests/PrimeFactorsUnitTests.cs
@@ -0,0 +1,89 @@
+using System;
+using Xunit;
+using PrimeFactorsLib;
+
+namespace PrimeFactorsTests
+{
+    public class PrimeFactorsUnitTests
+    {
+        [Fact]
+        public void TestPrimeFactorsOf4()
+        {
+            // arrange
+            int a = 4;
+            string expected = "2 x 2";
+
+            var primeFactors = new Primes();
+
+            // act
+            string actual = primeFactors.PrimeFactors(a);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestPrimeFactorsOf7()
+        {
+            // arrange
+            int a = 7;
+            string expected = "2";
+
+            var primeFactors = new Primes();
+
+            // act
+            string actual = primeFactors.PrimeFactors(a);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestPrimeFactorsOf30()
+        {
+            // arrange
+            int a = 30;
+            string expected = "5 x 3 x 2";
+
+            var primeFactors = new Primes();
+
+            // act
+            string actual = primeFactors.PrimeFactors(a);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestPrimeFactorsOf40()
+        {
+            // arrange
+            int a = 40;
+            string expected = "5 x 2 x 2 x 2";
+
+            var primeFactors = new Primes();
+
+            // act
+            string actual = primeFactors.PrimeFactors(a);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestPrimeFactorsOf50()
+        {
+            // arrange
+            int a = 50;
+            string expected = "5 x 5 x 2";
+
+            var primeFactors = new Primes();
+
+            // act
+            string actual = primeFactors.PrimeFactors(a);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
