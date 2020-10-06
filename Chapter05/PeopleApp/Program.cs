d381b0592d9e2877e38932081c95de4192f1f073
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-06 01:36:56 -0400

    Call method OptionalParameters skipping 2nd parameter

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -113,6 +113,8 @@ namespace PeopleApp
             WriteLine(edixon.OptionalParameters("Jump!", 98.5));
 
             WriteLine(edixon.OptionalParameters(number: 52.7, command: "Hide!"));
+
+            WriteLine(edixon.OptionalParameters("Poke!", active: false));
         }
     }
 }
