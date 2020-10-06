bfc43200a91c0661e395939dc5e3c87bbd6b4aa5
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-06 01:52:38 -0400

    Add property Origin

--- Chapter05/PacktLibrary/PersonAutoGen.cs
+++ Chapter05/PacktLibrary/PersonAutoGen.cs
@@ -2,6 +2,15 @@ namespace Packt.Shared
 {
     public partial class Person
     {
-        
+        // a property defined using C# 1 - 5 syntax 
+        public string Origin
+        {
+            get
+            {
+                return $"{Name} was born on {HomePlanet}";
+            }
+        }
+
+
     }
 }
