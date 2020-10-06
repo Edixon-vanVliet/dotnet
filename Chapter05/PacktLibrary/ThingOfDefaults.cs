089e04adc2cf2f8dfec4388130cb486222b1f6f8
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-06 00:59:53 -0400

    Change default as in C# 7.1 and later

--- Chapter05/PacktLibrary/ThingOfDefaults.cs
+++ Chapter05/PacktLibrary/ThingOfDefaults.cs
@@ -9,13 +9,13 @@ namespace Packt.Shared
         public DateTime When;
         public string Name;
         public List<Person> People;
-        
+
         public ThingOfDefaults()
         {
-            Population = default(int); // C# 2.0 and later
-            When = default(DateTime);
-            Name = default(string);
-            People = default(List<Person>);
+            Population = default; // C# 7.1 and later
+            When = default;
+            Name = default;
+            People = default;
         }
     }
 }
