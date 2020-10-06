6cf61a1e98287c9a5c3f486091a282fd18e5943b
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-06 02:35:28 -0400

    Use indexer

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -153,10 +153,18 @@ namespace PeopleApp
             );
 
             sam.FavoritePrimaryColor = "Red";
-            
+
             WriteLine(
                 $"{sam.Name}'s favorite primary color is {sam.FavoritePrimaryColor}."
             );
+
+            sam.Children.Add(new Person { Name = "Charlie" });
+            sam.Children.Add(new Person { Name = "Ella" });
+
+            WriteLine($"Sam's first child is {sam.Children[0].Name}");
+            WriteLine($"Sam's second child is {sam.Children[1].Name}");
+            WriteLine($"Sam's first child is {sam[0].Name}");
+            WriteLine($"Sam's second child is {sam[1].Name}");
         }
     }
 }
