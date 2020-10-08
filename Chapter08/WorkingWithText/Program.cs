2d73bcb8a06e072ba59c345f89c3e2cabc3acffe
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 10:23:09 -0400

    Create app WorkingWithText

---
+++ Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
