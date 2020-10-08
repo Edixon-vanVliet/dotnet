991a74396557c98a6ebf1106a49388c08f0fa7ba
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 10:25:59 -0400

    Display first and third character of a string

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -9,6 +9,8 @@ namespace WorkingWithText
         {
             string city = "London";
             WriteLine($"{city} is {city.Length} characters long.");
+
+            WriteLine($"First char is {city[0]} and third is {city[2]}.");
         }
     }
 }
