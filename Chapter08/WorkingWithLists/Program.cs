8582715acba753901e440b259c96a4119931296b
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 11:16:28 -0400

    Use List

--- Chapter08/WorkingWithLists/Program.cs
+++ Chapter08/WorkingWithLists/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
 
 namespace WorkingWithLists
 {
@@ -6,7 +9,35 @@ namespace WorkingWithLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cities = new List<string>();
+
+            cities.Add("London");
+            cities.Add("Paris");
+            cities.Add("Milan");
+
+            WriteLine("Initial list");
+            foreach (string city in cities)
+            {
+                WriteLine($"  {city}");
+            }
+
+            WriteLine($"The first city is {cities[0]}.");
+            WriteLine($"The last city is {cities[cities.Count - 1]}.");
+
+            cities.Insert(0, "Sydney");
+            WriteLine("After inserting Sydney at index 0");
+            foreach (string city in cities)
+            {
+                WriteLine($"  {city}");
+            }
+
+            cities.RemoveAt(1);
+            cities.Remove("Milan");
+            WriteLine("After removing two cities");
+            foreach (string city in cities)
+            {
+                WriteLine($" {city}");
+            }
         }
     }
 }
