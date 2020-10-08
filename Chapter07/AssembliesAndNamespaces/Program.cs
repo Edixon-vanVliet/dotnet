8329dfb878c0fdf6687ccbbba64048dbfcabdf50
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 00:37:32 -0400

    Declare a new XDocument

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace AssembliesAndNamespaces
 {
@@ -6,7 +7,7 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var doc = new XDocument();
         }
     }
 }
