5ffc4a988214f0246a32d074ba8555e8b85d2c3e
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 14:43:06 -0400

    Use Dictionary

--- Chapter08/WorkingWithDictionaries/Program.cs
+++ Chapter08/WorkingWithDictionaries/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
 
 namespace WorkingWithDictionaries
 {
@@ -6,7 +9,19 @@ namespace WorkingWithDictionaries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var keywords = new Dictionary<string, string>();
+
+            keywords.Add("int", "32-bit integer data type");
+            keywords.Add("long", "64-bit integer data type");
+            keywords.Add("float", "Single precision floating point number");
+
+            WriteLine("Keywords and their definitions");
+            foreach (KeyValuePair<string, string> item in keywords)
+            {
+                WriteLine($"  {item.Key}: {item.Value}");
+            }
+
+            WriteLine($"The definition of long is {keywords["long"]}");
         }
     }
 }
