ec88d4a0044694415fd158b0c625d4eff5adda74
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 01:16:02 -0400

    Add method IsValidPassword

--- Chapter07/SharedLibrary/StringExtensions.cs
+++ Chapter07/SharedLibrary/StringExtensions.cs
@@ -10,5 +10,11 @@ namespace Packt.Shared
                 input, @"^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)$"
             );
         }
+
+        public static bool IsValidPassword(this string input)
+        {
+            // minimum of eight valid characters
+            return Regex.IsMatch(input, "^[a-zA-Z0-9_-]{8,}$");
+        }
     }
 }
