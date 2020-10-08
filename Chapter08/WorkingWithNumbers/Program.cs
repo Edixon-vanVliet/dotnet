74a5b54f6e300fc9eeb650054c0c35f2426701d2
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 10:17:43 -0400

    Display a large number

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -10,8 +10,10 @@ namespace WorkingWithNumbers
         static void Main(string[] args)
         {
             var largest = ulong.MaxValue;
-
             WriteLine($"{largest,40:n0}");
+
+            var atomsInTheUniverse = BigInteger.Parse("123456789012345678901234567890");
+            WriteLine($"{atomsInTheUniverse,40:n0}");
         }
     }
 }
