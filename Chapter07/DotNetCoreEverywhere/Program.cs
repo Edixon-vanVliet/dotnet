e326bcdeb974a862466564ce43e8c77793010cce
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-08 00:46:47 -0400

    Display a message

--- Chapter07/DotNetCoreEverywhere/Program.cs
+++ Chapter07/DotNetCoreEverywhere/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace DotNetCoreEverywhere
 {
@@ -6,7 +7,7 @@ namespace DotNetCoreEverywhere
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("I can run everywhere!");
         }
     }
 }
