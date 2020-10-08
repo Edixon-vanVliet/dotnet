c87662ae0a31ce7778cd39611d6ae955d392967d
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-07 20:54:46 -0400

    Create class Shape

---
+++ Chapter06/Exercise02/Shape.cs
@@ -0,0 +1,9 @@
+namespace Exercise02
+{
+    public class Shape
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float Area { get; set; }
+    }
+}
