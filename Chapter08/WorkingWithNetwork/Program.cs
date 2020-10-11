3428ad4a7309933650f5dead82280fe1c2774f9f
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-10 20:28:06 -0400

    Create app WorkingWithNetwork

---
+++ Chapter08/WorkingWithNetwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
