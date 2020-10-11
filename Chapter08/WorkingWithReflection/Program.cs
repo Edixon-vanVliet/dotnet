e550db7155fff86fdcf7f4acd2b64e9aa0e53f9a
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-10-10 23:20:58 -0400

    Display all information about the custom Coder attribute

--- Chapter08/WorkingWithReflection/Program.cs
+++ Chapter08/WorkingWithReflection/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Linq;
 
 using static System.Console;
 
@@ -27,6 +28,39 @@ namespace WorkingWithReflection
             var company = assembly.GetCustomAttribute
              <AssemblyCompanyAttribute>();
             WriteLine($" Company: {company.Company}");
+
+            WriteLine();
+            WriteLine($"* Types:");
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                WriteLine();
+                WriteLine($"Type: {type.FullName}");
+
+                MemberInfo[] members = type.GetMembers();
+                foreach (MemberInfo member in members)
+                {
+                    WriteLine(
+                        "{0}: {1} ({2})",
+                        member.MemberType,
+                        member.Name,
+                        member.DeclaringType.Name
+                    );
+
+                    var coders = member
+                        .GetCustomAttributes<CoderAttribute>()
+                        .OrderByDescending(c => c.LastModified);
+                    foreach (CoderAttribute coder in coders)
+                    {
+                        WriteLine(
+                            "-> Modified by {0} on {1}",
+                            coder.Coder,
+                            coder.LastModified.ToShortDateString()
+                        );
+                    }
+                }
+            }
         }
 
         [Coder("Edixon van Vliet", "10 October 2020")]
