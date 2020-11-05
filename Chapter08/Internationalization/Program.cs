9ea1a74923a03c5ba05cba0f475d7b8cf8a1b9e8
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-11-05 18:09:52 -0400

    Display name, date of birth and earnings

--- Chapter08/Internationalization/Program.cs
+++ Chapter08/Internationalization/Program.cs
@@ -44,9 +44,21 @@ namespace Internationalization
 
             Write("Enter your date of birth: ");
             string dob = ReadLine();
-            
+
             Write("Enter your salary: ");
             string salary = ReadLine();
+
+            DateTime date = DateTime.Parse(dob);
+            int minutes = (int)DateTime.Today.Subtract(date).TotalMinutes;
+            decimal earns = decimal.Parse(salary);
+
+            WriteLine(
+                "{0} was born on a {1:dddd}, is {2:N0} minutes old, and earns {3:C}",
+                name,
+                date,
+                minutes,
+                earns
+            );
         }
     }
 }
