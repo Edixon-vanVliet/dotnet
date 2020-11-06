a2834778cfd85fef77e48ffa4b288dbcd07650d5
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-11-05 20:22:14 -0400

    Output match of regular expression and input of user

--- Chapter08/Exercise02/Program.cs
+++ Chapter08/Exercise02/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
+using static System.Console;
 
 namespace Exercise02
 {
@@ -6,7 +9,26 @@ namespace Exercise02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                Write("Enter a regular expression (or press ENTER to use the default): ");
+                string regex = ReadLine();
+
+                if (string.IsNullOrEmpty(regex))
+                {
+                    regex = "[1-9]+";
+                }
+
+                Write("Enter some input: ");
+                string input = ReadLine();
+
+                var regularExpression = new Regex(regex);
+
+                WriteLine($"{input} matches {regex}? {regularExpression.IsMatch(input)}");
+
+                WriteLine("Press ESC to end or any key to try again.");
+            }
+            while (ReadKey().Key != ConsoleKey.Escape);
         }
     }
 }
